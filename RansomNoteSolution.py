25bb6a0bbaba8fe61750ca063add782ab5ebf4a4
Matthew Ferraro <91623055+mferraro22@users.noreply.github.com>
2022-09-12 01:57:38 -0400

    Add files via upload

---
+++ RansomNoteSolution.py
@@ -0,0 +1,22 @@
+# Matthew Ferraro
+# HackerRank Solution
+# Hash Tables: Ransom Note
+
+from collections import Counter
+
+def checkMagazine(magazine, note):
+
+	"""
+	The goal was to see which words were used in the note
+	from the magazineto try to catch the criminal behind
+	the ransom note, including keeping track of duplicate words.
+	"""
+
+    # Write your code here
+    m = Counter(magazine)
+    n = Counter(note)
+    sep_elements = n - m
+    if list(sep_elements.elements()) == []:
+        print("Yes")
+    else:
+        print("No")
